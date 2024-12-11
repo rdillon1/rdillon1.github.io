6f7a40ff0abeda8a96c629da70384be20bb84468
rdillon1 <robbiedillon04@gmail.com>
2024-12-11 12:38:23 -0600

    downloaded data

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-<script>
-  
+function downloadData() {
+  event.preventDefault();
+  var startdate = document.getElementById("startdate").value;
+  var enddate = document.getElementById("enddate").value;
+  alert(startdate);
+  alert(enddate);
+}
